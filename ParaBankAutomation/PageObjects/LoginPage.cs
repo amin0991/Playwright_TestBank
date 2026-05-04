9e73a1598d66fd65607e579cfc367efe91a7ba57
amin0991 <67398845+amin0991@users.noreply.github.com>
2026-05-04 12:43:49 +0100

    fix(automated): N8N Auto-Fix Build #28 [N8N-Agent]

--- ParaBankAutomation/PageObjects/LoginPage.cs
+++ ParaBankAutomation/PageObjects/LoginPage.cs
@@ -1,99 +1,25 @@
-using ParaBankAutomation.Base;
-using ParaBankAutomation.Pages;
-using System;
-using TechTalk.SpecFlow;
+using Microsoft.Playwright;
 
-namespace ParaBankAutomation.StepDefinitions
+namespace ParaBankAutomation.PageObjects
 {
-    [Binding]
-    public class UserSetupSteps
+    public class LoginPage: BasePage
     {
-        private readonly ScenarioContext _scenarioContext;
-        private readonly LoginPage _loginPage;
-        private readonly OverviewPage _overviewPage;
-        private readonly RegisterPage _registerPage;
-        private readonly AccountsOverviewPage _accountsOverviewPage;
-        private readonly OpenNewAccountPage _openNewAccountPage;
-        private readonly RequestLoanPage _requestLoanPage;
-        private readonly BillPayPage _billPayPage;
-        private readonly FindTransactionsPage _findTransactionsPage;
-        private readonly TransferFundsPage _transferFundsPage;
-        private readonly UpdateContactInfoPage _updateContactInfoPage;
+        private ILocator UsernameInput => _page.Locator("[name='username']");
+        private ILocator PasswordInput => _page.Locator("[name='password']");
+        private ILocator LoginButton => _page.Locator("[value='Log In']");
 
-        public UserSetupSteps(ScenarioContext scenarioContext, LoginPage loginPage, OverviewPage overviewPage,
-            RegisterPage registerPage, AccountsOverviewPage accountsOverviewPage, OpenNewAccountPage openNewAccountPage,
-            RequestLoanPage requestLoanPage, BillPayPage billPayPage, FindTransactionsPage findTransactionsPage,
-            TransferFundsPage transferFundsPage, UpdateContactInfoPage updateContactInfoPage)
+        public LoginPage(IPage page) : base(page) { }
+
+        public async Task Login(string username, string password)
         {
-            _scenarioContext = scenarioContext;
-            _loginPage = loginPage;
-            _overviewPage = overviewPage;
-            _registerPage = registerPage;
-            _accountsOverviewPage = accountsOverviewPage;
-            _openNewAccountPage = openNewAccountPage;
-            _requestLoanPage = requestLoanPage;
-            _billPayPage = billPayPage;
-            _findTransactionsPage = findTransactionsPage;
-            _transferFundsPage = transferFundsPage;
-            _updateContactInfoPage = updateContactInfoPage;
+            await UsernameInput.FillAsync(username);
+            await PasswordInput.FillAsync(password);
+            await LoginButton.ClickAsync();
         }
 
-
-        [Given(@"I am on the login page")]
-        public void GivenIAmOnTheLoginPage()
+        public async Task Logout()
         {
-            _loginPage.GoTo();
-        }
-
-        [Given(@"I am logged in as a new user with username ""(.*)"" and password ""(.*)""")]
-        public void GivenIAmLoggedInAsANewUserWithUsernameAndPassword(string username, string password)
-        {
-            _registerPage.GoTo();
-            _registerPage.FillFormAndRegister(username, password);
-            _scenarioContext["username"] = username;
-            _scenarioContext["password"] = password;
-        }
-
-        [Given(@"I am logged in with username ""(.*)"" and password ""(.*)""")]
-        public void GivenIAmLoggedInWithUsernameAndPassword(string username, string password)
-        {
-            _loginPage.GoTo();
-            _loginPage.Login(username, password);
-            _scenarioContext["username"] = username;
-            _scenarioContext["password"] = password;
-        }
-
-        [When(@"I register a new user with first name ""(.*)"", last name ""(.*)"", address ""(.*)"", city ""(.*)"", state ""(.*)"", zip code ""(.*)"", phone ""(.*)"", ssn ""(.*)"", username ""(.*)"" and password ""(.*)"")]
-        public void WhenIRegisterANewUserWithFirstNameLastNameAddressCityStateZipCodePhoneSsnUsernameAndPassword(string firstName, string lastName, string address, string city, string state, string zipCode, string phone, string ssn, string username, string password)
-        {
-            _registerPage.FillFormAndRegister(firstName, lastName, address, city, state, zipCode, phone, ssn, username, password);
-            _scenarioContext["username"] = username;
-            _scenarioContext["password"] = password;
-        }
-
-        [When(@"I log in with username ""(.*)"" and password ""(.*)"")]
-        public void WhenILogInWithUsernameAndPassword(string username, string password)
-        {
-            _loginPage.Login(username, password);
-        }
-
-        [Then(@"I should be logged out")]
-        public void ThenIShouldBeLoggedOut()
-        {
-            _overviewPage.ClickLogoutLink();
-            _loginPage.AssertPageIsVisible();
-        }
-
-        [Then(@"I should see a logout link")]
-        public void ThenIShouldSeeALogoutLink()
-        {
-            _overviewPage.AssertLogoutLinkIsVisible();
-        }
-
-        [When(@"I click the logout link")]
-        public void WhenIClickTheLogoutLink()
-        {
-            _overviewPage.ClickLogoutLink();
+            await _page.Locator("text=Log Out").ClickAsync();
         }
     }
 }
